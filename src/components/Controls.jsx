9cdcadb9094e204fb34520436bb591a18205833d
Parminder Sanghera <gambit@Bilgas-MacBook-Pro.local>
2017-02-10 14:37:40 +0000

    fix bootstrap errors

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Button} from 'react-bootstrap'
-// import '../css/styles.scss'
+//import '../css/styles.scss'
 
 class Controls extends React.Component {
 
--- src/components/container/App.jsx
+++ src/components/container/App.jsx
@@ -1,13 +1,10 @@
 'use strict'
 
 import React from 'react'
-// import '../../css/styles.scss'
-import './app.css'
-import Controls from 'Controls'
-import Board from 'Board'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import {Button, Grid, DropdownButton, MenuItem} from 'react-bootstrap'
-
-// import Game from '../modules/game.js'
+import MainNav from '../MainNav'
+import './app.css' 
 
 class App extends React.Component {
   constructor(props) {
@@ -19,54 +16,60 @@ class App extends React.Component {
       game: ''
     }
   }
-    render() {
+  render() {
     return (
       <Grid fluid>
-            <div className="header clearfix">
-              <nav>
-                <ul className="nav nav-pills pull-right">
-                  <li role="presentation" className="active"><a href="#">Home</a></li>
-                  <li role="presentation"><a href="#">About</a></li>
-                  <li role="presentation"><a href="#">Contact</a></li>
-                </ul>
-              </nav>
-              <h3 className="text-muted">Project name</h3>
-            </div>
+        <div className="header clearfix">
+          <MainNav/>
+        </div>
 
-            <div className="jumbotron">
-              <h1>Jumbotron heading</h1>
-              <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <p><a className="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
-            </div>
+        <div className="jumbotron">
+          <h1>Jumbotron heading</h1>
+          <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+            Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+            fermentum massa justo sit amet risus.</p>
+          <p>
+            <a className="btn btn-lg btn-success" href="#" role="button">Sign up today</a>
+          </p>
+        </div>
 
-            <div className="row">
-              <div className="col-lg-12">
-                <Button>test</Button>
-                <DropdownButton title="Dropdown" id="bg-nested-dropdown">
-                  <MenuItem eventKey="1">Dropdown link</MenuItem>
-                  <MenuItem eventKey="2">Dropdown link</MenuItem>
-                </DropdownButton>
-              </div>
-            </div>            
+        <div className="row">
+          <div className="col-lg-12">
+            <Button>test</Button>
+            <DropdownButton title="Dropdown" id="bg-nested-dropdown">
+              <MenuItem eventKey="1">Dropdown link</MenuItem>
+              <MenuItem eventKey="2">Dropdown link</MenuItem>
+            </DropdownButton>
+          </div>
+        </div>
 
-              <div className="dropdown">
-                <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-                <span className="caret"></span></button>
-                <ul className="dropdown-menu">
-                  <li><a href="#">HTML</a></li>
-                  <li><a href="#">CSS</a></li>
-                  <li><a href="#">JavaScript</a></li>
-                </ul>
-              </div>
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            data-toggle="dropdown">Dropdown Example
+            <span className="caret"></span>
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">HTML</a>
+            </li>
+            <li>
+              <a href="#">CSS</a>
+            </li>
+            <li>
+              <a href="#">JavaScript</a>
+            </li>
+          </ul>
+        </div>
 
+        <footer className="footer">
+          <p>&copy; 2016 Company, Inc.</p>
+        </footer>
 
-            <footer className="footer">
-              <p>&copy; 2016 Company, Inc.</p>
-            </footer>
-
-          </Grid>
-      )
-    }
+      </Grid>
+    )
+  }
 }
 
 module.exports = App
